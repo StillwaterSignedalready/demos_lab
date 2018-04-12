f463235c292181873bfc9a3b063fee541bd1b734
Wayne <doublecross@yeah.net>
2018-04-12 21:20:54 +0800

    whatever

--- learnNode/src/main.js
+++ learnNode/src/main.js
@@ -2,25 +2,40 @@
 // var http = require('http')
 // var path = require('path')
 // var fs = require('fs')
+// var curry = require('lodash/curry')
+// var _ = require('ramda')
+var _ = require('ramda')
+var curry = _.curry;
+var reduce = _.reduce;
+var add = _.add;
+var toLower = _.toLower;
+var split = _.split;
+var replace = _.replace;
+var map = _.map;
+var compose = _.compose;
 
-// console.log(math);
+var CARS = [
+  {name: "Ferrari FF", horsepower: 660, dollar_value: 700000, in_stock: true},
+  {name: "Spyker C12 Zagato", horsepower: 650, dollar_value: 648000, in_stock: false},
+  {name: "Jaguar XKR-S", horsepower: 550, dollar_value: 132000, in_stock: false},
+  {name: "Audi R8", horsepower: 525, dollar_value: 114200, in_stock: false},
+  {name: "Aston Martin One-77", horsepower: 750, dollar_value: 1850000, in_stock: true},
+  {name: "Pagani Huayra", horsepower: 700, dollar_value: 1300000, in_stock: false}
+];
 
-// 加入􀬇􀟲nextTick()的回调函数
-process.nextTick(function () {
-  console.log('nextTick􁃽􀗿执行1');
+var trace = curry(function(tag, x){
+  console.log(tag, x);
+  return x;
 });
-process.nextTick(function () {
-  console.log('nextTick􁃽􀗿执行2');
-});
-// 加入􀬇􀟲setImmediate()的回调函数
-setImmediate(function () {
-  console.log('setImmediate􁃽􀗿执行1');
-  // 进入􁀰􀙒循环
-  process.nextTick(function () {
-    console.log('􀴽势􀖭入');
-  });
-});
-setImmediate(function () {
-  console.log('setImmediate􁃽􀗿执行2');
-});
-console.log('正常执行');
+// 使用帮助函数 _average 重构 averageDollarValue 使之成为一个组合
+// array -> (array的平均值 :number)
+var _average = function(xs) { return reduce(add, 0, xs) / xs.length; }; // <- 无须改动
+
+// var averageDollarValue = function(cars) {
+//   var dollar_values = map(function(c) { return c.dollar_value; }, cars);
+//   return _average(dollar_values);
+// };
+
+var averageDollarValue = compose(_average, map(function(c) { return c.dollar_value; }));
+
+console.log(averageDollarValue(CARS))
