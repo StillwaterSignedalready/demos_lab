1cbb1f1af7090afec8a01bdf2d440fd95adbc2df
Wayne <doublecross@yeah.net>
2018-06-23 18:04:32 +0800

    -> {}[]

--- handleSQL/src/sqlToJson.js
+++ handleSQL/src/sqlToJson.js
@@ -2,6 +2,14 @@ const fs = require('fs')
 
 const file = fs.readFileSync('./target/citys.sql', 'utf-8')
 const nodes = file.match(/{[^}]+}/g)
+    .map(str => str.replace(/{|}/g, ''))
+    .map(str => str.split(','))
+    .map(arr => ({
+        code: arr[0],
+        name: arr[1],
+        parentCode: arr[2],
+        grandParent: arr[3]
+    }))
 
-
+    
 console.log(nodes);
