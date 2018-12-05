c110cc6d6bdcc37e50c67524419a004c0dd3b79f
stillwaterSignedalready <doublecross@yeah.net>
2018-12-05 23:03:39 +0800

    partion

--- learnWhateverByNode/src/sorters.js
+++ learnWhateverByNode/src/sorters.js
@@ -43,28 +43,54 @@ function mergeSort(arr) { // arr: number[]
   }
 }
 
-const size = 1000;
-const arr1 = [...new Array(2000)].map(_ => Math.floor(Math.random()*size));
-const arr2 = [...new Array(2000)].map(_ => Math.floor(Math.random()*size));
-
-let start = new Date().getTime();
-bubbleSort(arr1);
-let end = new Date().getTime();
-console.log('bubbleSort: ', end - start)
-
-start = new Date().getTime();
-mergeSort(arr2);
-end = new Date().getTime();
-console.log('mergeSort: ', end - start)
 
 
 // 斐波那契数列
 
 // 快速排序
-
-function partition(arr, sIndex, eIndex) {
-  const pivot = arr[sIndex];
-  // iterate arr
-  // 大于pivot放左边 反之放右边
-
+/** 
+ * 大于pivot放左边 反之放右边
+ * @param {Array} arr 
+ * @param {number} sIndex 
+ * @param {number} eIndex 
+ * @param {number} pivotIndex between sIndex and eIndex
+ */
+function partition(arr, sIndex, eIndex, pivotIndex) {
+  const pivot = arr[pivotIndex];
+  // pivot 移动到末尾
+  arr[pivotIndex] = arr[eIndex];
+  arr[eIndex] = pivot;
+  let tmp = null;
+  let storeIndex = sIndex; // 动态，在左边的都是确认小于pivot的
+  for (let i = sIndex; i < eIndex; i++) {
+    if (arr[i] < pivot) {
+      // 交换 i 和 storeIndex
+      tmp = arr[i];
+      arr[i] = arr[storeIndex];
+      arr[storeIndex] = tmp;
+      storeIndex += 1; // 一旦交换马上右移,确保增量
+    }
+  }
+  // 此时 storeIndex大于pivot 交换 pivot所在index
+  arr[eIndex] = arr[storeIndex];
+  arr[storeIndex] = pivot;
+  return storeIndex
 }
+
+
+const size = 1000;
+const arr = [5, 6, 1, 2, 10, 3, 4, 8, 9, 7, 0];
+partition(arr, 0, 10, 0)
+console.log('\n ---final: ', JSON.stringify(arr))
+// const arr1 = [...new Array(2000)].map(_ => Math.floor(Math.random()*size));
+// const arr2 = [...new Array(2000)].map(_ => Math.floor(Math.random()*size));
+
+// let start = new Date().getTime();
+// bubbleSort(arr1);
+// let end = new Date().getTime();
+// console.log('bubbleSort: ', end - start)
+
+// start = new Date().getTime();
+// mergeSort(arr2);
+// end = new Date().getTime();
+// console.log('mergeSort: ', end - start)
